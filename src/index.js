ed42cb67c5627803b089aa3c97e50f2502c5d8f1
Antonina Dubich <dubich.t@gmail.com>
2020-10-10 19:56:59 +0300

    feat: morze function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = ''
+  expr = expr.replace(/\*{10}/g, ' ')
+  let words = expr.split(' ')
+  
+  for(let i = 0; i < words.length; i++){
+    let subWords = words[i].replace(/.{2}/g, '$& ').trim().split(' ');
+    for(let j = 0; j < subWords.length; j++){
+      if(subWords[j] === '10') {
+        subWords[j] = '.'
+      } else if(subWords[j] === '11') {
+        subWords[j] = '-'
+      }
+    }
+    subWords = subWords.join('').replace(/0/g, ' ').split(' ');
+    
+    subWords = subWords.filter((item) => {
+      if (item !== '') {
+        return item;
+      }
+    })
+
+    for(let key in MORSE_TABLE) {
+
+      for(let n = 0; n < subWords.length; n++) {
+        if(subWords[n] === key){
+          subWords[n] = MORSE_TABLE[key]
+        }
+      }
+    }
+
+    result += subWords.join('') + ' '
+  }
+
+
+  return result.trim()
 }
 
 module.exports = {
