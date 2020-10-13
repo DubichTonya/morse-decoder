6a9785fcea20ba32a11c298fd938f0867230a961
Antonina Dubich <dubich.t@gmail.com>
2020-10-13 19:30:53 +0300

    fix: correct one bugs

--- src/index.js
+++ src/index.js
@@ -38,41 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    let result = ''
-  expr = expr.replace(/\*{10}/g, ' ')
-  let words = expr.split(' ')
-  
-  for(let i = 0; i < words.length; i++){
-    let subWords = words[i].replace(/.{2}/g, '$& ').trim().split(' ');
-    for(let j = 0; j < subWords.length; j++){
-      if(subWords[j] === '10') {
-        subWords[j] = '.'
-      } else if(subWords[j] === '11') {
-        subWords[j] = '-'
+  let result = ''
+expr = expr.replace(/\*{10}/g, ' ')
+let words = expr.split(' ')
+
+for(let i = 0; i < words.length; i++){
+  let subWords = words[i].replace(/.{2}/g, '$& ').trim().split(' ');
+  for(let j = 0; j < subWords.length; j++){
+    if(subWords[j] === '10') {
+      subWords[j] = '.'
+    } else if(subWords[j] === '11') {
+      subWords[j] = '-'
+    }
+  }
+  subWords = subWords.join('').replace(/0/g, ' ').split(' ');
+
+  subWords = subWords.filter((item) => {
+    if (item !== '') {
+      return item;
+    }
+  })
+
+  for(let key in MORSE_TABLE) {
+
+    for(let n = 0; n < subWords.length; n++) {
+      if(subWords[n].length > 6) {
+        subWords = subWords[n].match(/.{5}/g)
+      }
+      if(subWords[n] === key){
+        subWords[n] = MORSE_TABLE[key]
       }
     }
-    subWords = subWords.join('').replace(/0/g, ' ').split(' ');
-    
-    subWords = subWords.filter((item) => {
-      if (item !== '') {
-        return item;
-      }
-    })
-
-    for(let key in MORSE_TABLE) {
-
-      for(let n = 0; n < subWords.length; n++) {
-        if(subWords[n] === key){
-          subWords[n] = MORSE_TABLE[key]
-        }
-      }
-    }
-
-    result += subWords.join('') + ' '
   }
 
+  result += subWords.join('') + ' '
+}
 
-  return result.trim()
+
+return result.trim()
 }
 
 module.exports = {
